c94a528e9300de77c98b529118fa90a7e179985a
Saravana Krishnan J <saravanakrishnan16@gmail.com>
2025-01-26 21:09:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to display a message when the button is clicked
+function displayMessage() {
+  const messageElement = document.getElementById("message");
+  messageElement.textContent = "Hello, you've successfully clicked the button!";
+}
