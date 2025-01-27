bcbfab18332a626e9dfbadb2eb648b9a80b29794
Saravana Krishnan J <saravanakrishnan16@gmail.com>
2025-01-27 22:28:56 +0530

    Initial commit

--- post-merge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Copy all files to the Apache htdocs folder
-cp -r C:/Users/Hi/Desktop/website/* C:/Users/Hi/Downloads/httpd-2.4.62-240904-win64-VS17/Apache24/htdocs/
-echo "Website files updated in htdocs!"
